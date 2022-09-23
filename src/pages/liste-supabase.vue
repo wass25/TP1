89078f9b73b75192425f9fad5cd27d89b39c9238
wass25 <wassel.azzouz@edu.univ-fcomte.fr>
2022-09-23 10:45:57 +0200

    liste-supabase

---
+++ src/pages/liste-supabase.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import card from "../components/Card.vue";
+
+const res = await fetch("/maisons.json");
+const listeMaisons = await res.json();
+
+</script>
+<template>
+    <div class="flex">
+        <div v-for="uneMaison in listeMaisons" :key="uneMaison.titre">
+            <card class="w-1/2" v-bind="uneMaison" />
+        </div>
+    </div>
+</template>
--- src/supabase.ts
+++ src/supabase.ts
@@ -4,3 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// Pour tester dans la console, dé-commenter la ligne qui suit :
+globalThis.supabase = supabase
